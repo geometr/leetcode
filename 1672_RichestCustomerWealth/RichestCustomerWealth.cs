30244883b865ec25e8cc2515bd066220924e7320
Kurochkin Konstatin <geometr.sinc@gmail.com>
2024-04-01 23:32:42 +0300

    Quest 1678 complete!

---
+++ 1672_RichestCustomerWealth/RichestCustomerWealth.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1672_RichestCustomerWealth
+{
+    public static class RichestCustomerWealth
+    {
+        public static int Solution(int[][] accounts)
+        {
+            int maxWealth = 0;
+            foreach (int []account in accounts)
+            {
+                int wealth = account.Sum();
+                if (wealth > maxWealth) maxWealth = wealth;
+            }
+            return maxWealth;
+        }
+    }
+}
---
+++ 1672_RichestCustomerWealthTests/RichestCustomerWealthTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1672_RichestCustomerWealth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1672_RichestCustomerWealth.Tests
+{
+    [TestClass()]
+    public class RichestCustomerWealthTests
+    {
+        [TestMethod()]
+        public void SolutionTest1()
+        {
+            int[][] accounts = { new int[]{ 1, 2, 3 }, new int[] { 3, 2, 1 } };
+            int correct = 6;
+            Assert.IsTrue(checkSolution(accounts, correct));
+        }
+        [TestMethod()]
+        public void SolutionTest2()
+        {
+            int[][] accounts = { new int[] { 1, 5 }, new int[] { 7,3 }, new int[] { 3,5 } };
+            int correct = 10;
+            Assert.IsTrue(checkSolution(accounts, correct));
+        }
+        [TestMethod()]
+        public void SolutionTest3()
+        {
+            int[][] accounts = { new int[] { 2, 8, 7 }, new int[] { 7, 1, 3 }, new int[] { 1, 9, 5 } };
+            int correct = 17;
+            Assert.IsTrue(checkSolution(accounts, correct));
+        }
+
+        private Boolean checkSolution(int[][] accounts, int correct) {
+            int result = RichestCustomerWealth.Solution(accounts);
+            return result == correct;
+        }
+    }
+}
