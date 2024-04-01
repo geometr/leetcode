1cd39e82666e451f7a308ed4de196bdc869d6e56
Kurochkin Konstatin <geometr.sinc@gmail.com>
2024-04-01 23:39:14 +0300

    Miss namespace naming. Fix it

--- 02_AddTwoNumbers/AddTwoNumbers.cs
+++ 02_AddTwoNumbers/AddTwoNumbers.cs
@@ -1,4 +1,4 @@
-﻿using GGerom.ListNode;
+﻿using Geometr.ListNode;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- 02_AddTwoNumbersTests/AddTwoNumbersTests.cs
+++ 02_AddTwoNumbersTests/AddTwoNumbersTests.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using GGerom.ListNode;
+using Geometr.ListNode;
 
 namespace _02_AddTwoNumbers.Tests
 {
