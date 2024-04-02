dd6bc85d75dd05dfcc76d6ab8b508720c9f4ae90
Kurochkin Konstatin <geometr.sinc@gmail.com>
2024-04-02 11:54:22 +0300

    Quest 1342 complete

---
+++ 1342_NumberOfStepsToReduceANumberToZero/NumberOfSteps.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1342_NumberOfStepsToReduceANumberToZero
+{
+    public static class NumberOfSteps
+    {
+        public static int Solution(int num) {
+            int answer = 0;
+            while (num != 0)
+            {
+                if ((num & 1) == 0)
+                {
+                    num = num >> 1;
+                }
+                else
+                {
+                    num--;
+                }
+                answer++;
+            }
+            return answer;
+        }
+    }
+}
---
+++ 1342_NumberOfStepsToReduceANumberToZeroTests/NumberOfStepsTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1342_NumberOfStepsToReduceANumberToZero;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1342_NumberOfStepsToReduceANumberToZero.Tests
+{
+    [TestClass()]
+    public class NumberOfStepsTests
+    {
+        [TestMethod()]
+        public void SolutionTest1()
+        {
+            int question = 14;
+            int correct = 6;
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        [TestMethod()]
+        public void SolutionTest2()
+        {
+            int question = 8;
+            int correct = 4;
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        [TestMethod()]
+        public void SolutionTest3()
+        {
+            int question = 123;
+            int correct = 12;
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        private Boolean checkSolution(int question, int correct)
+        {
+            int answer = NumberOfSteps.Solution(question);
+            return answer == correct;
+        }
+    }
+}
