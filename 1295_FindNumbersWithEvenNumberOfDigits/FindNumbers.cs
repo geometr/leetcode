2c842b1949ba61e0c813989833c753aa1d7c105b
Kurochkin Konstatin <geometr.sinc@gmail.com>
2024-04-03 22:44:01 +0300

    Quest 1295 complete

---
+++ 1295_FindNumbersWithEvenNumberOfDigits/FindNumbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1295_FindNumbersWithEvenNumberOfDigits
+{
+    public static class FindNumbers
+    {
+        public static int Solution(int[] nums) {
+            int numberOfEvenNumbers = 0;
+            foreach (int num in nums) {
+                int number = num;
+                int numOfDigit = 1;
+                while (number > 9) {
+                    number = number / 10;
+                    numOfDigit++;
+                }
+                if (numOfDigit % 2 == 0) numberOfEvenNumbers++;
+            }
+            return numberOfEvenNumbers;
+        }
+    }
+}
---
+++ 1295_FindNumbersWithEvenNumberOfDigitsTests/FindNumbersTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1295_FindNumbersWithEvenNumberOfDigits;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1295_FindNumbersWithEvenNumberOfDigits.Tests
+{
+    [TestClass()]
+    public class FindNumbersTests
+    {
+
+        [TestMethod()]
+        public void SolutionTest1()
+        {
+            int[] question = { 12, 345, 2, 6, 7896 };
+            int correct = 2;
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        [TestMethod()]
+        public void SolutionTest2()
+        {
+            int[] question = { 555, 901, 482, 1771 };
+            int correct = 1;
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+
+        private bool checkSolution(int[] question, int correct) {
+            int answer = FindNumbers.Solution(question);
+            if (correct == answer) return true;
+            return false;
+        }
+    }
+}
