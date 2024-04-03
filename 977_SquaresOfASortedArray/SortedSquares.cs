ed04bb71172af3106ce8e1edd425d2eae3d7da5a
Kurochkin Konstatin <geometr.sinc@gmail.com>
2024-04-03 23:12:56 +0300

    Quest 977 complete

---
+++ 977_SquaresOfASortedArray/SortedSquares.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _977_SquaresOfASortedArray
+{
+    public static class SortedSquares
+    {
+        public static int[] Solution(int[] nums) {
+            int[] res = new int[nums.Length];
+            int left = 0;
+            int right = nums.Length - 1;
+            int index = right;
+            while (left <= right) {
+                int leftVal = nums[left] * nums[left];
+                int rightVal = nums[right] * nums[right];
+                if (leftVal > rightVal) {
+                    res[index] = leftVal;
+                    left++;
+                } else {
+                    res[index] = rightVal;
+                    right--;
+                }
+                index--;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ 977_SquaresOfASortedArrayTests/SortedSquaresTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _977_SquaresOfASortedArray;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _977_SquaresOfASortedArray.Tests
+{
+    [TestClass()]
+    public class SortedSquaresTests
+    {
+        [TestMethod()]
+
+        public void SolutionTest1()
+        {
+            int[] question = { -4, -1, 0, 3, 10 };
+            int[] correct = { 0, 1, 9, 16, 100 };
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        public void SolutionTest2()
+        {
+            int[] question = { -7, -3, 2, 3, 11 };
+            int[] correct = { 4, 9, 9, 49, 121 };
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        private bool checkSolution(int[] question, int[] correct) {
+            int[] answer = SortedSquares.Solution(question);
+            for (int i = 0; i < correct.Length; i++) {
+                if (correct[i] != answer[i]) return false;
+            }
+            return true;
+        }
+    }
+}
