ed69cf26061ac59cd19441a9cbb2d3b9a3ed6406
Kurochkin Konstatin <geometr.sinc@gmail.com>
2024-04-04 16:08:08 +0300

    Quest 88 complete

---
+++ 88_MergeSortedArray/Merge.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _88_MergeSortedArray
+{
+    public static class Merge
+    {
+        public static void Solution(int[] nums1, int m, int[] nums2, int n)
+        {
+            int pointer1 = m-1;
+            int pointer2 = n-1;
+            int index = nums1.Length-1;
+            while (index >= 0) {
+                int val1 = -9999999;
+                if (pointer1 >= 0) { 
+                    val1 = nums1[pointer1];
+                }
+                int val2 = -9999999;
+                if (pointer2 >= 0)
+                {
+                    val2 = nums2[pointer2];
+                }
+                if (val1 > val2) {
+                    nums1[index] = val1;
+                    if (pointer1 >= 0)
+                    {
+                        pointer1--;
+                    }
+                }
+                else
+                {
+                    nums1[index] = val2;
+                    if (pointer2 >= 0)
+                    {
+                        pointer2--;
+                    }
+                }
+                index--;
+            }
+        }
+    }
+}
---
+++ 88_MergeSortedArrayTests/MergeTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _88_MergeSortedArray;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _88_MergeSortedArray.Tests
+{
+    [TestClass()]
+    public class MergeTests
+    {
+        [TestMethod()]
+        public void SolutionTest1()
+        {
+            int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+            int m = 3;
+            int[] nums2 = { 2, 5, 6 };
+            int n = 3;
+
+            int[] correct = { 1, 2, 2, 3, 5, 6 };
+
+            Merge.Solution(nums1, m, nums2, n);
+            Assert.IsTrue(checkSolution(nums1,correct));
+        }
+        [TestMethod()]
+public void SolutionTest2()
+        {
+            int[] nums1 = { 1 };
+            int m = 1;
+            int[] nums2 = {};
+            int n = 0;
+
+            int[] correct = { 1 };
+
+            Merge.Solution(nums1, m, nums2, n);
+            Assert.IsTrue(checkSolution(nums1, correct));
+        }
+        [TestMethod()]
+        public void SolutionTest3()
+        {
+            int[] nums1 = { 0 };
+            int m = 0;
+            int[] nums2 = { 1 };
+            int n = 1;
+
+            int[] correct = { 1 };
+
+            Merge.Solution(nums1, m, nums2, n);
+            Assert.IsTrue(checkSolution(nums1, correct));
+        }
+        private bool checkSolution(int[] nums1, int[] correct) {
+            for (int i=0; i < nums1.Length; i++)
+            {
+                if (nums1[i] != correct[i]) return false;
+            }
+            return true;
+        }
+    }
+}
