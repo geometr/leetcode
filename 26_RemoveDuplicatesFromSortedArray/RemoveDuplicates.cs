026697b2cc4f4ba51e692671c718a9e922860fc3
Kurochkin Konstatin <geometr.sinc@gmail.com>
2024-04-05 11:03:27 +0300

    Quest 26 complete

---
+++ 26_RemoveDuplicatesFromSortedArray/RemoveDuplicates.cs
@@ -0,0 +1,23 @@
+﻿namespace _26_RemoveDuplicatesFromSortedArray
+{
+    public static class RemoveDuplicates
+    {
+        public static int Solution(int[] nums) {
+            if (nums.Length == 1) return 1;
+            int newLen = 1;
+            int index = 1;
+            int insertPosition = 1;
+            while(index < nums.Length)
+            {
+                if (nums[index] != nums[index - 1]) { 
+                    nums[insertPosition] = nums[index];
+                    insertPosition++;
+                    newLen++;
+                }
+                index++;
+            }
+            return newLen;
+
+        }
+    }
+}
---
+++ 26_RemoveDuplicatesFromSortedArrayTests/RemoveDuplicatesTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _26_RemoveDuplicatesFromSortedArray;
+
+
+namespace _26_RemoveDuplicatesFromSortedArray.Tests
+{
+    [TestClass()]
+    public class RemoveDuplicatesTests
+    {
+        [TestMethod()]
+        public void SolutionTest1()
+        {
+            int[] question = { 1, 1, 2 };
+            int correct = 2;
+            int[] correctArr = { 1, 2 };
+            Assert.IsTrue(checkSolution(question, correct, correctArr));
+        }
+        [TestMethod()]
+        public void SolutionTest2()
+        {
+            int[] question = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            int correct = 5;
+            int[] correctArr = { 0, 1, 2, 3, 4 };
+            Assert.IsTrue(checkSolution(question, correct, correctArr));
+        }
+        private bool checkSolution(int[] question, int correct, int[] correctArr) {
+            int answer = RemoveDuplicates.Solution(question);
+            if (answer != correct) return false;
+            for (int i = 0; i < correctArr.Length; i++)
+            {
+                if (question[i] != correctArr[i]) return false;
+            };
+
+            return true;
+        }
+    }
+}
