8044f98a4337ca9abb40af75b681737315724b54
Kurochkin Konstatin <geometr.sinc@gmail.com>
2024-04-05 12:48:00 +0300

    Quest 941 complete

---
+++ 941_ValidMountainArray/ValidMountainArray.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _941_ValidMountainArray
+{
+    public static class ValidMountainArray
+    {
+        public static bool Solution(int[] arr)
+        {
+            bool inc = true;
+            bool leftPlato = false;
+            int max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] == arr[i - 1]) return false;
+                if ((arr[i] > arr[i - 1])&&inc)
+                {
+                    leftPlato = true;
+                    if ((arr[i] > max)&&inc)
+                    {
+                        max = arr[i];
+                    }
+                    else
+                    {
+                        inc = false;
+                        continue;
+                    }
+                }
+                if (arr[i] < arr[i - 1])
+                {
+                    if (inc && !leftPlato) return false;  
+                    if (inc) inc = false;
+                    continue;
+                }
+                if (!inc&&(arr[i] > arr[i - 1]))
+                {
+                 
+                    return false;
+                }
+            };
+            if (inc) return false;
+            if (!leftPlato) return false;
+            return true;
+        }
+    }
+}
---
+++ 941_ValidMountainArrayTests/ValidMountainArrayTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _941_ValidMountainArray;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _941_ValidMountainArray.Tests
+{
+    [TestClass()]
+    public class ValidMountainArrayTests
+    {
+        [TestMethod()]
+        public void SolutionTest1()
+        {
+            int[] question = {2,1};
+            Assert.IsFalse(ValidMountainArray.Solution(question));
+        }
+        [TestMethod()]
+        public void SolutionTest2()
+        {
+            int[] question = { 3, 5, 5};
+            Assert.IsFalse(ValidMountainArray.Solution(question));
+        }
+        [TestMethod()]
+        public void SolutionTest3()
+        {
+            int[] question = { 0, 3, 2, 1 };
+            Assert.IsTrue(ValidMountainArray.Solution(question));
+        }
+        [TestMethod()]
+        public void SolutionTest4()
+        {
+            int[] question = { 2,0,2 };
+            Assert.IsFalse(ValidMountainArray.Solution(question));
+        }
+        [TestMethod()]
+        public void SolutionTest5()
+        {
+            int[] question = {0,1,2,1,2 };
+            Assert.IsFalse(ValidMountainArray.Solution(question));
+        }
+    }
+}
