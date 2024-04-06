45dd13422d9485ec232183eac9265376f58e3119
Kurochkin Konstatin <geometr.sinc@gmail.com>
2024-04-06 19:39:50 +0300

    Quest 905 complete

---
+++ 905_SortArrayByParity/SortArrayByParity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _905_SortArrayByParity
+{
+    public static class SortArrayByParity
+    {
+        public static int[] Solution(int[] nums) {
+            int rightCursor = nums.Length - 1;
+            int leftCursor = 0;
+            while (leftCursor < rightCursor) { 
+           
+                if ((nums[leftCursor]%2) != 0)
+                {
+                    bool needInsert = true;
+                    while (needInsert && rightCursor>leftCursor) {
+                        if (nums[rightCursor] % 2 != 0)
+                        {
+                            rightCursor--;
+                        }
+                        else 
+                        {
+                            int value = nums[leftCursor];
+                            nums[leftCursor] = nums[rightCursor];
+                            nums[rightCursor] = value;
+                            needInsert = false;
+                            leftCursor++;
+                            rightCursor--;
+                        }
+                    }
+                }
+                else
+                {
+                    leftCursor++;
+                }
+            }
+            return nums;
+        }
+    }
+}
---
+++ 905_SortArrayByParityTests/SortArrayByParityTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _905_SortArrayByParity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _905_SortArrayByParity.Tests
+{
+    [TestClass()]
+    public class SortArrayByParityTests
+    {
+        [TestMethod()]
+        public void SolutionTest1()
+        {
+            int[] question = { 3, 1, 2, 4 };
+            int[] correct = { 4, 2, 1, 3 };
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        [TestMethod()]
+        public void SolutionTest2()
+        {
+            int[] question = { 0 };
+            int[] correct = { 0 };
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        private bool checkSolution(int[] question, int[] correct) {
+            int[] answer = SortArrayByParity.Solution(question);
+            if (answer.Length != correct.Length) return false;
+            for (int i = 0; i < answer.Length; i++)
+            {
+                if (answer[i] != correct[i]) return false;
+            }
+            return true;
+        }
+    }
+}
