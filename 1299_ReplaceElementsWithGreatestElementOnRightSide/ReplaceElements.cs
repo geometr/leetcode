c7017e5ba8fdd17d660bf8182d73efa192e12261
Kurochkin Konstatin <geometr.sinc@gmail.com>
2024-04-06 16:18:00 +0300

    Quest 1299 complete

---
+++ 1299_ReplaceElementsWithGreatestElementOnRightSide/ReplaceElements.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1299_ReplaceElementsWithGreatestElementOnRightSide
+{
+    public static class ReplaceElements
+    {
+        public static int[] Solution(int[] arr)
+        {
+            int max = arr[arr.Length - 1];
+            arr[arr.Length - 1] = -1;
+            for (int i = arr.Length-2; i >= 0; i--)
+            {
+                int currentVal = arr[i];
+                arr[i] = max;
+                if (currentVal > max) max = currentVal;
+            }
+            return arr;
+        }
+    }
+}
---
+++ 1299_ReplaceElementsWithGreatestElementOnRightSideTests/ReplaceElementsTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1299_ReplaceElementsWithGreatestElementOnRightSide;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1299_ReplaceElementsWithGreatestElementOnRightSide.Tests
+{
+    [TestClass()]
+    public class ReplaceElementsTests
+    {
+        [TestMethod()]
+        public void SolutionTest1()
+        {
+            int[] question = { 17, 18, 5, 4, 6, 1 };
+            int[] correct = { 18, 6, 6, 6, 1, -1 };
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        [TestMethod()]
+        public void SolutionTest2()
+        {
+            int[] question = { 400 };
+            int[] correct = { -1 };
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        private bool checkSolution(int[] question, int[] correct)
+        {
+            int[] answer = ReplaceElements.Solution(question);
+            if (answer.Length == 0) return false;
+            for (int i = 0; i < answer.Length; i++)
+            {
+                if (answer[i] != correct[i]) return false;
+            }
+            return true;
+        }
+    }
+}
