4bf51a2043d8af9319de0974bf55a3557d8453a8
Kurochkin Konstatin <geometr.sinc@gmail.com>
2024-04-06 18:06:05 +0300

    Quest 283 complete

---
+++ 283_MoveZeroes/MoveZeroes.cs
@@ -0,0 +1,26 @@
+﻿namespace _283_MoveZeroes
+{
+    public static class MoveZeroes
+    {
+        public static void Solution(int[] nums) {
+            int cursor = 0;
+            int zeroNum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    zeroNum++;
+                }
+                else {
+                    nums[cursor] = nums[i];
+                    cursor++;
+                    
+                }
+            }
+            for (int i = nums.Length-zeroNum; i < nums.Length; i++)
+            {
+                nums[i] = 0;
+            }
+        }
+    }
+}
---
+++ 283_MoveZeroesTests/MoveZeroesTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _283_MoveZeroes;
+
+namespace _283_MoveZeroes.Tests
+{
+    [TestClass()]
+    public class MoveZeroesTests
+    {
+        [TestMethod()]
+        public void SolutionTest1()
+        {
+            int[] question = { 0, 1, 0, 3, 12 };
+            int[] correct = { 1,3,12,0,0};
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        [TestMethod()]
+        public void SolutionTest2()
+        {
+            int[] question = { 0};
+            int[] correct = {0 };
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        [TestMethod()]
+        public void SolutionTest3()
+        {
+            int[] question = { 0,1 };
+            int[] correct = { 1,0 };
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        private bool checkSolution(int[] question, int[] correct) {
+            MoveZeroes.Solution(question);
+            for (int i = 0; i < question.Length; i++)
+            {
+                if (correct[i] != question[i]) return false;
+            }
+            return true;
+        }
+    }
+}
