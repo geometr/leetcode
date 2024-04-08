022be2ec005685b2d3952bb8e4bd4b2318ba5fd6
Kurochkin Konstatin <geometr.sinc@gmail.com>
2024-04-08 12:11:09 +0300

    Quest 1051 complete

---
+++ 1051_HeightChecker/HeightChecker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace _1051_HeightChecker
+{
+    public static class HeightChecker
+    {
+        public static int Solution(int [] heights)
+        {
+            int[] sortered = new int[heights.Length];
+            Array.Copy(heights, 0, sortered, 0, heights.Length);
+            Array.Sort(sortered);
+            int diff = 0;
+            for (int i = 0; i < heights.Length; i++) {
+                if (heights[i] != sortered[i]) diff++;
+            }
+            return diff;
+        }
+    }
+}
---
+++ 1051_HeightCheckerTests/HeightCheckerTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1051_HeightChecker;
+
+
+namespace _1051_HeightChecker.Tests
+{
+    [TestClass()]
+    public class HeightCheckerTests
+    {
+        [TestMethod()]
+        public void SolutionTest1()
+        {
+            int[] question = { 1, 1, 4, 2, 1, 3 };
+            int correct = 3;
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        [TestMethod()]
+        public void SolutionTest2()
+        {
+            int[] question = { 5,1,2,3,4 };
+            int correct = 5;
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        [TestMethod()]
+        public void SolutionTest3()
+        {
+            int[] question = { 1,2,3,4,5 };
+            int correct = 0;
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        private bool checkSolution(int[] question, int correct){
+            int answer = HeightChecker.Solution(question);
+            if (answer == correct) return true;
+            return false;
+        }
+    }
+}
