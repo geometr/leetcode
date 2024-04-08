f5398c59a5e67fdb59b0e73745ea634da98e065c
Kurochkin Konstatin <geometr.sinc@gmail.com>
2024-04-08 16:38:04 +0300

    Quest 414 complete

---
+++ 414_ThirdMaximumNumber/ThirdMax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _414_ThirdMaximumNumber
+{
+    public static class ThirdMax
+    {
+        public static int Solution(int[] nums) {
+            int max = nums[0];
+            int? med = null;
+            int? low = null;
+            for (int i = 1; i < nums.Length; i++) {
+                int num = nums[i];
+                
+                if ((num == max) || (num == med) || (num == low)) continue;
+
+                if (num < max is false) (max, med, low) = (num, max, med);
+                else if (num < med is false) (med, low) = (num,med);
+                    else if (num < low is false ) low = num;
+            }
+            return low ?? max;
+        }
+    }
+}
---
+++ 414_ThirdMaximumNumberTests/ThirdMaxTests.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _414_ThirdMaximumNumber;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _414_ThirdMaximumNumber.Tests
+{
+    [TestClass()]
+    public class ThirdMaxTests
+    {
+
+        [TestMethod()]
+        public void SolutionTest1()
+        {
+            int[] question = { 2, 2, 3, 1 };
+            int correct = 1;
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+
+        [TestMethod()]
+        public void SolutionTest2()
+        {
+            int[] question = { 1, 2 };
+            int correct = 2;
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+
+        [TestMethod()]
+        public void SolutionTest3()
+        {
+            int[] question = { 3, 2, 1 };
+            int correct = 1;
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        [TestMethod()]
+        public void SolutionTest4()
+        {
+            int[] question = { 1, 1, 2 };
+            int correct = 2;
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        [TestMethod()]
+        public void SolutionTest5()
+        {
+            int[] question = { 5,2,2 };
+            int correct = 5;
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        [TestMethod()]
+        public void SolutionTest6()
+        {
+            int[] question = { 14 };
+            int correct = 14;
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        [TestMethod()]
+        public void SolutionTest7()
+        {
+            int[] question = {1,2,2,5,3,5};
+            int correct = 2;
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        [TestMethod()]
+        public void SolutionTest8()
+        {
+            int[] question = {1,-2147483648,2};
+            int correct = -2147483648;
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        [TestMethod()]
+        public void SolutionTest9()
+        {
+            int[] question = { 15, 2, 4, 1, 3, 6, 0 };
+            int correct = 4;
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        private bool checkSolution(int[] question, int correct) {
+            int answer = ThirdMax.Solution(question);
+            if (answer == correct) return true;
+            return false;
+        }
+    }
+}
