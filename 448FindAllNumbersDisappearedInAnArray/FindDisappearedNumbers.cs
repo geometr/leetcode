bbc72d6780da48b5d1c8d10af4e9e7b9fa5225ba
Kurochkin Konstatin <geometr.sinc@gmail.com>
2024-04-08 19:23:08 +0300

    Quest 448 complete

---
+++ 448FindAllNumbersDisappearedInAnArray/FindDisappearedNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _448FindAllNumbersDisappearedInAnArray
+{
+    public static class FindDisappearedNumbers
+    {
+        public static IList<int> Solution(int[] nums) {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int index = Math.Abs(nums[i]) - 1;
+                if( nums[index]>0) nums[index] = -nums[index];
+            }
+            IList<int> returnLst = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > 0)
+                {
+                    returnLst.Add(i + 1);
+                }
+            }
+            return returnLst;
+        }
+    }
+}
---
+++ 448FindAllNumbersDisappearedInAnArrayTests/FindDisappearedNumbersTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _448FindAllNumbersDisappearedInAnArray;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _448FindAllNumbersDisappearedInAnArray.Tests
+{
+    [TestClass()]
+    public class FindDisappearedNumbersTests
+    {
+        [TestMethod()]
+        public void SolutionTest1()
+        {
+            int[] question = { 4,3,2,7,8,2,3,1};
+            IList<int> correct = new List<int> { 5,6 };
+            Assert.IsTrue(checkSolution(question,correct));
+        }
+        [TestMethod()]
+        public void SolutionTest2()
+        {
+            int[] question = { 1, 1};
+            IList<int> correct = new List<int> { 2 };
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        [TestMethod()]
+        public void SolutionTest3()
+        {
+            int[] question = { 4,3,2,7,8,2,3,1 };
+            IList<int> correct = new List<int> { 5,6 };
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        private bool checkSolution(int[] question, IList<int> correct)
+        {
+            IList<int> answer = FindDisappearedNumbers.Solution(question);
+            for (int i = 0; i < answer.Count; i++)
+            {
+                if (answer.ElementAt(i) != correct.ElementAt(i)) return false;
+            }
+            return true;
+        }
+    }
+}
