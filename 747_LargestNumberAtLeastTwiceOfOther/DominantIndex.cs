27dcb8eb56b80ef014cd1416395d16632235c9f4
Kurochkin Konstatin <geometr.sinc@gmail.com>
2024-04-11 10:30:24 +0300

    Quest 747 complete

---
+++ 747_LargestNumberAtLeastTwiceOfOther/DominantIndex.cs
@@ -0,0 +1,32 @@
+﻿namespace _747_LargestNumberAtLeastTwiceOfOther
+{
+    public static class DominantIndex
+    {
+        public static int Solution(int[] nums)
+        {
+            int max = nums[0];
+            int? second = null;
+            int maxPos = 0;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+
+                if (nums[i] > max)
+                {
+                    second = max;
+                    max = nums[i];
+                    maxPos = i;
+                }
+                else
+                {
+                    if (nums[i] > second || second ==null)
+                    {
+                        second = nums[i];
+                    }
+
+                }
+            }
+            return max>= second * 2  ? maxPos : -1;
+        }
+    }
+}
---
+++ 747_LargestNumberAtLeastTwiceOfOtherTests/DominantIndexTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _747_LargestNumberAtLeastTwiceOfOther;
+namespace _747_LargestNumberAtLeastTwiceOfOther.Tests
+{
+    [TestClass()]
+    public class DominantIndexTests
+    {
+        [TestMethod()]
+        public void SolutionTest1()
+        {
+            int[] nums = {3,6,1,0};
+            int correct = 1;
+            Assert.IsTrue(checkSolution(nums,correct));
+        }
+        [TestMethod()]
+        public void SolutionTest2()
+        {
+            int[] nums = { 1,2,3,4 };
+            int correct = -1;
+            Assert.IsTrue(checkSolution(nums, correct));
+        }
+        [TestMethod()]
+        public void SolutionTest3()
+        {
+            int[] nums = { 1, 0 };
+            int correct = 0;
+            Assert.IsTrue(checkSolution(nums, correct));
+        }
+
+        private bool checkSolution(int[] nums, int correct)
+        {
+            int answer = DominantIndex.Solution(nums);
+            if (answer == correct) return true;
+            return false;
+        }
+    }
+}
