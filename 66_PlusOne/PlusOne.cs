3f5383aed6d1c8b065f8fea4c63d2463707833f6
Kurochkin Konstatin <geometr.sinc@gmail.com>
2024-04-11 11:06:29 +0300

    Quest 66 complete

---
+++ 66_PlusOne/PlusOne.cs
@@ -0,0 +1,38 @@
+﻿namespace _66_PlusOne
+{
+    public static class PlusOne
+    {
+        public static int[] Solution(int[] digits) {
+            bool carry = true;
+            int index = digits.Length - 1;
+            while (carry)
+            {
+                digits[index]++;
+                if (digits[index] >= 10)
+                {
+                    digits[index] -= 10;
+                    carry = true;
+                }
+                else
+                {
+                    carry = false;
+                }
+                index--;
+
+                if (index < 0)
+                    break;
+            }
+            int[] result = digits;
+            if (index < 0 && carry)
+            {
+                result = new int[digits.Length + 1];
+                for (int i = 0; i < digits.Length; i++)
+                {
+                    result[i + 1] = digits[i];
+                }
+                result[0] = 1;
+            }
+            return result;
+        }
+    }
+}
---
+++ 66_PlusOneTests/PlusOneTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _66_PlusOne;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _66_PlusOne.Tests
+{
+    [TestClass()]
+    public class PlusOneTests
+    {
+        [TestMethod()]
+        public void PlusOneTest1()
+        {
+            int[] question = { 1, 2, 3 };
+            int[] correct = { 1, 2, 4 };
+            Assert.IsTrue(checkSolution(question,correct));
+        }
+        [TestMethod()]
+        public void PlusOneTest2()
+        {
+            int[] question = { 4,3,2,1 };
+            int[] correct = { 4,3,2,2 };
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        [TestMethod()]
+        public void PlusOneTest3()
+        {
+            int[] question = { 9 };
+            int[] correct = { 1,0 };
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        [TestMethod()]
+        public void PlusOneTest4()
+        {
+            int[] question = { 0 };
+            int[] correct = { 1 };
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        private bool checkSolution(int[] question,int[] correct) {
+            int[] answer = PlusOne.Solution(question);
+            for(int i = 0; i < correct.Length; i++)
+            {
+                if (answer[i] != correct[i]) return false;
+            }
+            return true;
+        }
+    }
+}
