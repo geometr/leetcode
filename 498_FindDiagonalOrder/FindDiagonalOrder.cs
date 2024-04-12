a968783646b7b1d3a2dd559837cb8a0ee2541ff4
Kurochkin Konstatin <geometr.sinc@gmail.com>
2024-04-12 11:30:02 +0300

    Quest 498 complete

---
+++ 498_FindDiagonalOrder/FindDiagonalOrder.cs
@@ -0,0 +1,57 @@
+﻿namespace _498_FindDiagonalOrder
+{
+    public static class FindDiagonalOrder
+    {
+        public static int[] Solution(int[][] mat)
+        {
+            int index = 0;
+            int indexX = 0;
+            int indexY = 0;
+            int maxX = mat[0].Length;
+            int maxY = mat.Length;
+            bool dir = true;
+            int[] result = new int[maxX * maxY];
+            while (index < result.Length) {
+               
+                if (indexX < 0 || indexX >= maxX|| indexY < 0 || indexY >= maxY) { 
+                    if (indexY == -1 && indexX == maxX)
+                    {
+                        indexY = 1;
+                        indexX = maxX-1;
+                    }
+                    if (indexX == -1 && indexY == maxY)
+                    {
+                        indexX = 1;
+                        indexY = maxY-1;
+                    }
+                    if (indexY < 0) indexY = 0;
+                    if (indexX < 0) indexX = 0;
+                    if (indexX >=maxX)
+                    {
+                        indexX = maxX-1;
+                        indexY += 2;
+                    }
+                    if (indexY >= maxY)
+                    {
+                        indexY = maxY - 1 ;
+                        indexX += 2;
+                    }
+                    dir = !dir;
+                }
+                result[index] = mat[indexY][indexX];
+                if (dir)
+                {
+                    indexX += 1;
+                    indexY -= 1;
+                }
+                else
+                {
+                    indexX -= 1;
+                    indexY += 1;
+                }
+                index++;
+            }
+            return result;
+        }
+    }
+}
---
+++ 498_FindDiagonalOrderTests/FindDiagonalOrderTests.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _498_FindDiagonalOrder;
+
+namespace _498_FindDiagonalOrder.Tests
+{
+    [TestClass()]
+    public class FindDiagonalOrderTests
+    {
+        [TestMethod()]
+        public void SolutionTest1()
+        {
+            int[][] question = new int[3][];
+            question[0] = new int[] { 1, 2, 3};
+            question[1] = new int[] { 4, 5, 6};
+            question[2] = new int[] { 7, 8, 9};
+            int[] correct = { 1, 2, 4, 7, 5, 3, 6, 8, 9 };
+            Assert.IsTrue(checkSolution(question,correct));
+        }
+        [TestMethod()]
+        public void SolutionTest2()
+        {
+            int[][] question = new int[2][];
+            question[0] = new int[] { 1, 2};
+            question[1] = new int[] { 3, 4};
+            int[] correct = { 1, 2, 3, 4};
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        [TestMethod()]
+        public void SolutionTest3()
+        {
+            int[][] question = new int[3][];
+            question[0] = new int[] { 2, 5 };
+            question[1] = new int[] { 8, 4 };
+            question[2] = new int[] { 0, -1 };
+            int[] correct = { 2,5,8,0,4,-1 };
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        [TestMethod()]
+        public void SolutionTest4()
+        {
+            int[][] question = new int[2][];
+            question[0] = new int[] { 3 };
+            question[1] = new int[] { 2 };
+            
+            int[] correct = { 3,2 };
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        [TestMethod()]
+        public void SolutionTest5()
+        {
+            int[][] question = new int[2][];
+            question[0] = new int[] { 2,5,8 };
+            question[1] = new int[] { 4,0,-1 };
+
+            int[] correct = { 2,5,4,0,8,-1 };
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+        [TestMethod()]
+        public void SolutionTest6()
+        {
+            int[][] question = new int[1][];
+            question[0] = new int[] { 1,2,3,4,5,6,7,8,9,10 };
+            
+
+            int[] correct = { 1,2,3,4,5,6,7,8,9,10};
+            Assert.IsTrue(checkSolution(question, correct));
+        }
+
+
+        private bool checkSolution(int[][] question, int[] correct)
+        {
+            int[] answer = FindDiagonalOrder.Solution(question);
+            if (answer.Length != correct.Length) return false;
+            for (int i = 0; i< answer.Length; i++)
+            {
+                if (answer[i] != correct[i]) return false;
+            }
+            return true;
+        }
+    }
+}
